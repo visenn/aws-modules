559ade3818a53e47f577dd8fd0f6d9a3fb6c755a
visenn <46842597+visenn@users.noreply.github.com>
2023-10-02 04:02:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "s3_bucket" {
 
   bucket_name = "vis-s3-example-2200"  # Change to your desired bucket name
   versioning_enabled = true  # Set to true to enable versioning
-  s3access_logs_bucket_arn = "arn:aws:s3:::aws-logs-612820001683-eu-west-1"  # Provide the S3 access logs bucket ARN if needed
+  s3access_logs_bucket_arn = "arn:aws:s3:::aws-logs-612820001683-eu-west-1/vis-s3-example-2200"  # Provide the S3 access logs bucket ARN if needed
   lifecycle_rule = {
     days_to_standard_ia = 30  # Set to the desired number of days to move to Standard-IA
     days_to_glacier = 60     # Set to the desired number of days to move to Glacier
