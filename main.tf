e026f560971c7aa565db3cdfe4a605c14c025250
visenn <46842597+visenn@users.noreply.github.com>
2023-10-02 04:37:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,24 @@ module "s3_bucket" {
   source = "./modules/"
 
   bucket_name = "vis-s3-example-2200"  # Change to your desired bucket name
-  versioning_enabled = true  # Set to true to enable versioning
-  s3access_logs_bucket_arn = "arn:aws:s3:::aws-logs-612820001683-eu-west-1/vis-s3-example-2200"  # Provide the S3 access logs bucket ARN if needed
-  lifecycle_rule = {
-    days_to_standard_ia = 30  # Set to the desired number of days to move to Standard-IA
-    days_to_glacier = 60     # Set to the desired number of days to move to Glacier
+  versioning = true  # Set to true to enable versioning
+  s3access = {
+    bucket = "arn:aws:s3:::aws-logs-612820001683-eu-west-1"
+    prefix = "vis-s3-example-2200"  
   }
-  cross_account_roles = {
+  lifecycle = {
+    days_to_standard_ia = -1  # Set to the desired number of days to move to Standard-IA
+    days_to_glacier = 3650     # Set to the desired number of days to move to Glacier
+  }
+  external_iam_roles = {
     role1 = {
-      role_arn      = "arn:aws:iam::976042434071:role/AdmRole4Viscon"
-      folder_path   = "example-folder1"
-      permissions   = "read"
+      arn      = "arn:aws:iam::976042434071:role/AdmRole4Viscon"
+      subfolder   = "example-folder1"
+      permissions   = "readonly"
     },
     role2 = {
-      role_arn      = "arn:aws:iam::121653596033:role/AdmRole4Viscon"
-      folder_path   = "example-folder2"
+      arn      = "arn:aws:iam::121653596033:role/AdmRole4Viscon"
+      subfolder   = "example-folder2"
       permissions   = "read_write"
     },
   } 
