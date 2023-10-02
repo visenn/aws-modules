ec92c3ff2580194c10fbfaca0ec36497b611d926
visenn <46842597+visenn@users.noreply.github.com>
2023-10-02 03:05:52 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+module "s3_bucket" {
+  source = "./modules/"
+
+  bucket_name = "vis-s3-example-2200"
+  
+  cross_account_roles = {
+    role1 = {
+      role_arn      = "arn:aws:iam::976042434071:role/AdmRole4Viscon"
+      folder_path   = "/folder1"
+      permissions   = ["s3:GetObject"]
+      access_types  = ["Allow"]
+    },
+    role2 = {
+      role_arn      = "arn:aws:iam::121653596033:role/AdmRole4Viscon"
+      folder_path   = "/folder2"
+      permissions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+      access_types  = ["Allow"]
+    }
+  }  
+  
+  s3_access_logs_bucket_arn = {
+    bucket_arn   = "arn:aws:s3:::aws-logs-612820001683-eu-west-1"
+    folder_path  = "vis-s3-example-2200/"
+  }
+}
