47702c61e15f9513dd6478bbc2d65f0db1086125
visenn <46842597+visenn@users.noreply.github.com>
2023-10-02 05:00:51 +0200

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -11,14 +11,15 @@ resource "aws_s3_bucket" "bucket" {
       }
     }
   }
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
   
-  # Block public access
-  public_access_block {
-    block_public_acls       = true
-    block_public_policy     = true
-    ignore_public_acls      = true
-    restrict_public_buckets = true
-  }
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_versioning" "bucket_versioning" {
