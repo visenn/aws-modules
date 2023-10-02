18b9fe82b21efe42f96483b7c8beaf69cf4ed774
visenn <46842597+visenn@users.noreply.github.com>
2023-10-02 03:51:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,15 @@
 module "s3_bucket" {
   source = "./modules/"
 
-  bucket_name = "vis-s3-example-2200"
-  kms_key_id = "arn:aws:kms:eu-west-1:612820001683:key/44a3332f-8b09-451d-a690-0ac477359a69"
-  
-  cross_account_roles = {
-    role1 = {
-      role_arn      = "arn:aws:iam::976042434071:role/AdmRole4Viscon"
-      folder_path   = "/folder1"
-      permissions   = ["s3:GetObject"]
-      access_types  = ["Allow"]
-    },
-    role2 = {
-      role_arn      = "arn:aws:iam::121653596033:role/AdmRole4Viscon"
-      folder_path   = "/folder2"
-      permissions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
-      access_types  = ["Allow"]
-    }
-  }  
-  
-  s3_access_logs_bucket_arn = {
-    bucket_arn   = "arn:aws:s3:::aws-logs-612820001683-eu-west-1"
-    folder_path  = "vis-s3-example-2200/"
+  bucket_name = "vis-s3-example-2200"  # Change to your desired bucket name
+  versioning_enabled = true  # Set to true to enable versioning
+  s3access_logs_bucket_arn = "arn:aws:s3:::aws-logs-612820001683-eu-west-1"  # Provide the S3 access logs bucket ARN if needed
+  lifecycle_rule = {
+    days_to_standard_ia = 30  # Set to the desired number of days to move to Standard-IA
+    days_to_glacier = 60     # Set to the desired number of days to move to Glacier
   }
+  cross_account_iam_roles = [
+    "arn:aws:iam::976042434071:role/AdmRole4Viscon",
+    "arn:aws:iam::121653596033:role/AdmRole4Viscon",
+  ]  # List of IAM roles to grant access from other AWS accounts
 }
